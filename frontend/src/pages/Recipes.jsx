2c1ce9c4bf3dc643c506310d7ea06f852a212033
Greg Schoenberg <gregschoenberg@gmail.com>
2022-09-02 14:25:21 -0700

    add endpoint url for recipe, env keys with vite, map titles

--- frontend/src/pages/Recipes.jsx
+++ frontend/src/pages/Recipes.jsx
@@ -1,29 +1,56 @@
+import { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import Axios from 'axios'
+import Spinner from '../components/Spinner'
 
 const Recipes = () => {
-  const APP_KEY = import.meta.env.EDAMAM_APP_KEY
-  const APP_ID = import.meta.env.EDAMAM_APP_ID
-console.log(APP_KEY)
+  const [recipeData, setRecipeData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(null);
+
+  let recipeTitles = null;
+
+  const APP_KEY = import.meta.env.VITE_EDAMAM_APP_KEY
+  const APP_ID = import.meta.env.VITE_EDAMAM_APP_ID
+
+  console.log("APP KEY: ", APP_KEY)
   const url = `https://api.edamam.com/api/recipes/v2?type=public&app_id=${APP_ID}&app_key=${APP_KEY}`
 
   const { groceries, isLoading, isError, message } = useSelector((state) => state.groceries)
 
   const getRecipes = async (ingredient) => {
-    const custom = `${url}&q=${ingredient}`
-    console.log("CUSTOM", custom)
-    const result = await Axios.get()
-    console.log(result.data)
+    const searchUrl = `${url}&q=${ingredient}`;
+    console.log("CUSTOM", searchUrl);
+    setLoading(true);
+    setError(false);
+    try {
+      const result = await Axios.get(searchUrl);
+      console.log(result.data);
+      setRecipeData(result.data);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setError(error);
+    }
   }
+
   const list = groceries.map(item => (
       <p key={item._id}>{item.text}</p>
   ))
-    console.log(list)
+
+  if (recipeData) {
+    recipeTitles = recipeData.hits.map((entry) => {
+      return <p key={entry.recipe.uri}>{entry.recipe.label}</p>;
+    });
+  }
+
   return (
     <>
-        <h1>Recipes</h1>
-        <ul>{list}</ul>
-        <button onClick={getRecipes}>get recipes</button>
+      <h1>Recipes</h1>
+      <ul>{list}</ul>
+      {loading && <Spinner />}
+      {recipeTitles && recipeTitles}
+      <button onClick={() => getRecipes(groceries[1].text)}>get recipes</button>
     </>
   )
 }
