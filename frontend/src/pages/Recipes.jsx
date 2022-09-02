5c9d8c310ae33e71d7d9b37cb9057057aa022d0b
Greg Schoenberg <gregschoenberg@gmail.com>
2022-09-01 22:15:56 -0700

    add url from edamam API

--- frontend/src/pages/Recipes.jsx
+++ frontend/src/pages/Recipes.jsx
@@ -1,8 +1,20 @@
 import { useSelector, useDispatch } from 'react-redux'
+import Axios from 'axios'
 
 const Recipes = () => {
+  const APP_KEY = import.meta.env.EDAMAM_APP_KEY
+  const APP_ID = import.meta.env.EDAMAM_APP_ID
+console.log(APP_KEY)
+  const url = `https://api.edamam.com/api/recipes/v2?type=public&app_id=${APP_ID}&app_key=${APP_KEY}`
+
   const { groceries, isLoading, isError, message } = useSelector((state) => state.groceries)
-  console.log(groceries)
+
+  const getRecipes = async (ingredient) => {
+    const custom = `${url}&q=${ingredient}`
+    console.log("CUSTOM", custom)
+    const result = await Axios.get()
+    console.log(result.data)
+  }
   const list = groceries.map(item => (
       <p key={item._id}>{item.text}</p>
   ))
@@ -11,7 +23,7 @@ const Recipes = () => {
     <>
         <h1>Recipes</h1>
         <ul>{list}</ul>
-        
+        <button onClick={getRecipes}>get recipes</button>
     </>
   )
 }
