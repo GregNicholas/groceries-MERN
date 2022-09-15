8701cd53358cfb186a4c4896a49cbb6d0785cc83
Greg Schoenberg <gregschoenberg@gmail.com>
2022-09-14 20:55:20 -0700

    add save, add recipe, and close recipe buttons

--- frontend/src/components/RecipeModal.jsx
+++ frontend/src/components/RecipeModal.jsx
@@ -1,19 +1,30 @@
+import { CgCloseO, CgHeart, CgPlayListAdd } from 'react-icons/cg'
+
 const RecipeModal = ({recipeInfo, closeModal}) => {
-    console.log(recipeInfo)
+    
+  const addIngredients = () => {
+    console.log("recipeInfo", recipeInfo.recipe.ingredientLines)
+  }
+
   return (
     <div 
         onClick={closeModal} 
         className="modal-background"
     >
         <div onClick={e=>e.stopPropagation()} className="recipe-card">
+            <div className="recipe-actions">
+                <div className="recipe-favorite"><CgHeart /><span className="icon-text">SAVE</span></div>
+                <div onClick={addIngredients} className="recipe-favorite"><CgPlayListAdd /><span className="icon-text">ADD ITEMS TO LIST</span></div>
+                <CgCloseO className="close-button" onClick={closeModal} />
+            </div>
             <h1 className="recipe-title">{recipeInfo.recipe.label}</h1>
             <div className="recipe-body">
-                <div class="recipe-image-container">
+                <div className="recipe-image-container">
                     <img className="recipe-image" src={recipeInfo.recipe.image} alt={recipeInfo.recipe.label} />
                 </div>
                 <ul>
-                    {recipeInfo.recipe.ingredientLines.map(ing => {
-                        return <li className="recipe-ingredient" key="ing">{ing}</li>
+                    {recipeInfo.recipe.ingredientLines.map((ing, i) => {
+                        return <li className="recipe-ingredient" key={i}>{ing}</li>
                     })}
                 </ul>
             </div>
